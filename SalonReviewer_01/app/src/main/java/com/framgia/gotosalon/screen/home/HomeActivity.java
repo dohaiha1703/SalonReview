6e05401efc29f41ebdc25324bd71d16cae70ca9e
Hai Ha <haiha@Hais-MBP.net.fpt>
2019-08-04 23:00:27 +0700

    edit home toolbar

--- SalonReviewer_01/app/src/main/java/com/framgia/gotosalon/screen/home/HomeActivity.java
+++ SalonReviewer_01/app/src/main/java/com/framgia/gotosalon/screen/home/HomeActivity.java
@@ -6,8 +6,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.framgia.gotosalon.R;
@@ -26,8 +28,9 @@ import java.util.List;
 public class HomeActivity extends BaseActivity implements View.OnClickListener, HomeContract.View {
     private static final String INTENT_KEY = "USER_ID";
     private String mUserId;
-    private ImageView mImageMenu;
-    private EditText mEditTextSearch;
+    private Button btnUpload;
+//    private EditText mEditTextSearch;
+    private TextView mText;
 
     @Override
     protected int getLayoutResource() {
@@ -41,11 +44,12 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener, 
         TabLayout tabLayout = findViewById(R.id.tab_home);
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
-        mEditTextSearch = findViewById(R.id.edit_search);
+//        mEditTextSearch = findViewById(R.id.edit_search);
         findViewById(R.id.image_back).setOnClickListener(this);
-        findViewById(R.id.image_search).setOnClickListener(this);
-        mImageMenu = findViewById(R.id.image_menu);
-        mImageMenu.setOnClickListener(this);
+//        findViewById(R.id.image_search).setOnClickListener(this);
+        btnUpload = findViewById(R.id.buttonUploadSalon);
+        btnUpload.setOnClickListener(this);
+        mText = findViewById(R.id.textView);
     }
 
     @Override
@@ -59,7 +63,9 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener, 
         presenter.setView(this);
         presenter.checkUserId(mUserId);
         if (mUserId == null){
-            mImageMenu.setVisibility(View.GONE);
+            btnUpload.setVisibility(View.GONE);
+        }else{
+            mText.setVisibility(View.GONE);
         }
     }
 
@@ -69,7 +75,7 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener, 
             case R.id.image_back:
                 finish();
                 break;
-            case R.id.image_menu:
+            case R.id.buttonUploadSalon:
                 if (mUserId != null){
                     Intent intent = new Intent(this, ManagerActivity.class);
                     intent.putExtra(INTENT_KEY, mUserId);
@@ -78,14 +84,14 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener, 
                 }
                 Toast.makeText(this, "Sign up to use this function", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.image_search:
-                Bundle bundle = new Bundle();
-                bundle.putString("data", mEditTextSearch.getText().toString().trim());
-                NewFragment newFragment = new NewFragment();
-                HotFragment hotFragment = new HotFragment();
-                newFragment.setArguments(bundle);
-                hotFragment.setArguments(bundle);
-                break;
+//            case R.id.image_search:
+//                Bundle bundle = new Bundle();
+//                bundle.putString("data", mEditTextSearch.getText().toString().trim());
+//                NewFragment newFragment = new NewFragment();
+//                HotFragment hotFragment = new HotFragment();
+//                newFragment.setArguments(bundle);
+//                hotFragment.setArguments(bundle);
+//                break;
         }
     }
 
